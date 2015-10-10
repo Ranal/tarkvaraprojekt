90178e909f3bfb41198c2169515f3f7e761d6e3e
Ranal Saron <ranalsaron@gmail.com>
2015-10-10 18:08:12 +0300

    Improvements

--- yhendus.go
+++ yhendus.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 )
 
-func main() {
+
+func yhendus(w http.ResponseWriter, req *http.Request) {
 	//db, err := sql.Open("postgres", "user=vcjthhaofvkqke dbname=dedgfoiefjhcdu sslmode=disable 
 	//	password=QXnZclsVqyZPU5C8Tn_ch81Qt2 host=ec2-54-217-238-100.eu-west-1.compute.amazonaws.com port=5432 ")
-	
-db, err := sql.Open("postgres", "postgres://vcjthhaofvkqke:QXnZclsVqyZPU5C8Tn_ch81Qt2@ec2-54-217-238-100.eu-west-1.compute.amazonaws.com:5432/dedgfoiefjhcdu")
+	db, err := sql.Open("postgres", "postgres://vcjthhaofvkqke:QXnZclsVqyZPU5C8Tn_ch81Qt2@ec2-54-217-238-100.eu-west-1.compute.amazonaws.com:5432/dedgfoiefjhcdu")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,4 +32,12 @@ if err != nil {
 	log.Fatal(err)
 }
 log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+
+
+func main() {
+	http.HandleFunc("/yhendus", yhendus)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
