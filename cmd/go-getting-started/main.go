0ddef278f98f925a0918f932e24e79e587bb9346
Ranal Saron <ranalsaron@gmail.com>
2015-10-11 19:57:34 +0300

    ++

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -32,7 +32,13 @@ func main() {
 
     //-----//
 
-    db, err := sql.Open("postgres", "postgres://vcjthhaofvkqke:QXnZclsVqyZPU5C8Tn_ch81Qt2@ec2-54-217-238-100.eu-west-1.compute.amazonaws.com:5432/dedgfoiefjhcdu")
+	router.Run(":" + port)
+}
+
+func yhendus(w http.ResponseWriter, req *http.Request) {
+	//db, err := sql.Open("postgres", "user=vcjthhaofvkqke dbname=dedgfoiefjhcdu sslmode=disable 
+	//	password=QXnZclsVqyZPU5C8Tn_ch81Qt2 host=ec2-54-217-238-100.eu-west-1.compute.amazonaws.com port=5432 ")
+	db, err := sql.Open("postgres", "postgres://vcjthhaofvkqke:QXnZclsVqyZPU5C8Tn_ch81Qt2@ec2-54-217-238-100.eu-west-1.compute.amazonaws.com:5432/dedgfoiefjhcdu")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -53,7 +59,4 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
-	//
-
-	router.Run(":" + port)
 }
