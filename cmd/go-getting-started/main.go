347cb10662cedb5b89dbe35d0ecb020136e3667f
Ranal Saron <ranalsaron@gmail.com>
2015-10-11 22:46:24 +0300

    ParseForm()

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/ranal/tarkvaraprojekt/tarkvaraprojekt/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	_ "github.com/ranal/tarkvaraprojekt/tarkvaraprojekt/Godeps/_workspace/src/github.com/lib/pq"
@@ -33,7 +34,20 @@ func dbFunc(c *gin.Context) {
 	c.String(http.StatusOK, fmt.Sprintf("Rida andmebaasi lisatud"))
 
 	defer rows.Close()
-	
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()  //Parse url parameters passed, then parse the response packet for the POST body (request body)
+    // attention: If you do not call ParseForm method, the following data can not be obtained form
+    fmt.Println(r.Form) // print information on server side.
+    fmt.Println("path", r.URL.Path)
+    fmt.Println("scheme", r.URL.Scheme)
+    fmt.Println(r.Form["url_long"])
+    for k, v := range r.Form {
+        fmt.Println("key:", k)
+        fmt.Println("val:", strings.Join(v, ""))
+    }
+    fmt.Fprintf(w, "Hello astaxie!") // write data to response
 }
 
 func main() {
@@ -59,7 +73,9 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
 	router.POST("/db", dbFunc)
+	router.POST("/test", sayhelloName)
 
 	router.Run(":" + port)
 }
