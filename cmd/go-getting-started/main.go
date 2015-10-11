512b0c2fb3faac10aa112c94202205ee97be486f
Ranal Saron <ranalsaron@gmail.com>
2015-10-11 22:06:43 +0300

    db conn

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	//"time"
 
 	"github.com/ranal/tarkvaraprojekt/tarkvaraprojekt/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	_ "github.com/ranal/tarkvaraprojekt/tarkvaraprojekt/Godeps/_workspace/src/github.com/lib/pq"
@@ -16,41 +14,17 @@ import (
 )
 
 var (
-	repeat int
 	db     *sql.DB = nil
 )
 
-func repeatFunc(c *gin.Context) {
-	var buffer bytes.Buffer
-	for i := 0; i < repeat; i++ {
-		buffer.WriteString("Hello from Go!")
-	}
-	c.String(http.StatusOK, buffer.String())
-}
-
 func dbFunc(c *gin.Context) {
+
+	if _, err := db.Exec("INSERT INTO andmed VALUES ('Uus', 'Rida', 'uuedread@gmail.com', 5009208)"); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error: %q", err))
+		return
+	}
 	/*
-	if _, err := db.Exec("CREATE TABLE andmed(eesnimi varchar(50), perekonnanimi varchar(50), email varchar(50), telefon integer)"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error creating database table: %q", err))
-		return
-	}
-	*/
-
-	if _, err := db.Exec("INSERT INTO andmed VALUES ('Ranal', 'Saron', 'ranalsaron@gmail.com', 5120628)"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error incrementing tick: %q", err))
-		return
-	}
-
-	//
-		if _, err := db.Exec("INSERT INTO andmed VALUES ('Dino', 'Saurus', 'jurassic@gmail.com', 500999)"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error incrementing tick: %q", err))
-		return
-	}
-	//
-
 	rows, err := db.Query("SELECT eesnimi FROM andmed")
 	if err != nil {
 		c.String(http.StatusInternalServerError,
@@ -59,34 +33,18 @@ func dbFunc(c *gin.Context) {
 	}
 
 	defer rows.Close()
-
-	/*
-	for rows.Next() {
-		var eesnimi string
-		if err := rows.Scan(&tick); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error scanning ticks: %q", err))
-			return
-		}
-		c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-	}
 	*/
 }
 
 func main() {
+
 	var err error
 	var errd error
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
-	}
-
-	tStr := os.Getenv("REPEAT")
-	repeat, err = strconv.Atoi(tStr)
-	if err != nil {
-		log.Print("Error converting $REPEAT to an int: %q - Using default", err)
-		repeat = 5
 	}
 
 	db, errd = sql.Open("postgres", os.Getenv("DATABASE_URL"))
@@ -102,12 +60,6 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
-
-	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-	})
-
-	router.GET("/repeat", repeatFunc)
 	router.GET("/db", dbFunc)
 
 	router.Run(":" + port)
