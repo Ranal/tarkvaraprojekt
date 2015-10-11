56a42db7a24c6b1cde1e92c8f962e5230033e792
Ranal Saron <ranalsaron@gmail.com>
2015-10-12 00:12:50 +0300

    +

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -71,7 +71,7 @@ func dbFunc(c *gin.Context) {
 
 	c.String(http.StatusOK, "Pilet ostetud! Nimi: %s %s | E-mail: %s | Telefon: %s", eesnimi, perekonnanimi, email, telefon)
 
-	if _, err := db.Exec("INSERT INTO andmed VALUES (eesnimi, perekonnanimi, email, telefon)"); err != nil {
+	if _, err := db.Exec("INSERT INTO andmed VALUES (%s, %s, %s, %s)"); err != nil {
 		c.String(http.StatusInternalServerError,
 			fmt.Sprintf("Error: %q", err))
 		return
