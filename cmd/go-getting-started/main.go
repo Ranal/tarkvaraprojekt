30f338a7d9d6f71bdaba21b8271dac3f80a99d05
Ranal Saron <ranalsaron@gmail.com>
2015-10-12 00:03:39 +0300

    +

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -69,7 +69,7 @@ func dbFunc(c *gin.Context) {
 	email := c.Query("email")
 	telefon := c.Query("telefon")
 
-	c.String(http.StatusOK, "Pilet ostetud!<br />Nimi: %s %s <br/>", eesnimi, perekonnanimi, "E-mail: %s <br/>", email, "Telefon: %s <br/>", telefon)
+	c.String(http.StatusOK, "Pilet ostetud! Nimi: %s %s", eesnimi, perekonnanimi, "E-mail: %s", email, "Telefon: %s", telefon)
 
 
 	/*
